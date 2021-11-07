c1bc3f8e3f2f816ac64218077270d3347bf381b2
nono303 <git@nono303.net>
2021-11-07 10:24:29 +0100

    add return type for SessionHandlerInterface implemented functions (PHP8.1)

--- MemcacheSessionHandlerPrepend.php
+++ MemcacheSessionHandlerPrepend.php
@@ -12,32 +12,33 @@
 			$this->prefix = isset( $options[ 'prefix' ] ) ? $options[ 'prefix' ] : 'sf2s-';
 		}
 
-		public function open( $savePath, $sessionName ) {
+		public function open( $savePath, $sessionName ) : bool {
 			$this->memcache->connect('127.0.0.1', 11211, $udp_port = 0, $persistent = true, $weight = 1, $timeout = 1, $retry_interval = 15);
 			return true;
 		}
 
-		public function close() {
+		public function close() : bool {
 			$this->memcache->close();
 			return true;
 		}
-		public function read( $sessionId ) {
+		public function read( $sessionId ) : string|false {
 			return $this->memcache->get( $this->prefix . $sessionId ) ? : '';
 		}
 
-		public function write( $sessionId, $data ) {
-			/* !! ttl is the third parameter with memcached.
-			http://php.net/manual/fr/memcache.set.php
-			http://php.net/manual/fr/memcached.set.php
+		public function write( $sessionId, $data ) : bool {
+			/* 
+				!! ttl is the third parameter with memcached.
+				http://php.net/manual/fr/memcache.set.php
+				http://php.net/manual/fr/memcached.set.php
 			*/
 			return $this->memcache->set( $this->prefix . $sessionId, $data, $flag = 0, time() + $this->ttl );
 		}
 
-		public function destroy( $sessionId ) {
+		public function destroy( $sessionId ) : bool {
 			return $this->memcache->delete( $this->prefix . $sessionId );
 		}
 
-		public function gc( $lifetime ) {
+		public function gc( $lifetime ) : int|false {
 			return true;
 		}
 	}
